251b7ee88d19eaab49e1f9644e45dbb18bcf201e
hstojic <hrvoje.stojic@protonmail.com>
2018-05-09 17:52:23 +0100

    first draft of the website

---
+++ README.md
@@ -0,0 +1,12 @@
+# This is the code for webpage of Estcado Tattoo
+
+It is automatically transformed by "Jekyll" into a static site.
+
+## License
+
+The following directories and their contents are Copyrighted by Nikola Boskovic. You may not reuse anything therein without my permission:
+
+* _posts/
+* images/
+
+All other directories and files are MIT Licensed. Feel free to use those files as you please. 
---
+++ js/portfolio.js
@@ -0,0 +1,17 @@
+$(function() {
+    console.log("blah2")
+    //Set up instafeed
+    var feed = new Instafeed({
+        clientId: '84561aa398304c9f95541003fe6d4abb',
+        target: 'instafeed',
+        get: 'tagged',
+        tagName: 'photographyportfolio',
+        links: true,
+        limit: 8,
+        sortBy: 'most-recent',
+        resolution: 'standard_resolution',
+        template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3"><div class="photo-box"><div class="image-wrap"><a href="{{link}}"><img src="{{image}}"></a><div class="likes">{{likes}} Likes</div></div><div class="description">{{caption}}<div class="date">{{model.date}}</div></div></div></div>'
+    });
+    feed.run();
+ 
+});
