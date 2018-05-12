151a4cb8d59720fd3ac9a5a0a7755f9ecf5dd638
hstojic <hrvoje.stojic@protonmail.com>
2018-05-12 22:06:27 +0100

    adapted portfolio code and styling of figures, now figure sizes are more uniform

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,16 +1,16 @@
 $(function() {
-    console.log("blah2")
-    //Set up instafeed
     var feed = new Instafeed({
         clientId: '84561aa398304c9f95541003fe6d4abb',
         target: 'instafeed',
-        get: 'tagged',
-        tagName: 'photographyportfolio',
+        // useHttp: true,
+        userId: 184745639,
+        accessToken: '184745639.84561aa.4104e9f3bba248e18c68a1685ed6cc45',
+        get: 'user',
         links: true,
-        limit: 8,
+        limit: 12,
         sortBy: 'most-recent',
         resolution: 'standard_resolution',
-        template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3"><div class="photo-box"><div class="image-wrap"><a href="{{link}}"><img src="{{image}}"></a><div class="likes">{{likes}} Likes</div></div><div class="description">{{caption}}<div class="date">{{model.date}}</div></div></div></div>'
+        template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3"><div class="photo-box"><div class="image-wrap"><a href="{{link}}"><img src="{{image}}" class="img-responsive"></a><div class="likes">{{likes}} Likes</div></div><div class="description">{{caption}}<div class="date">{{model.date}}</div></div></div></div>'
     });
     feed.run();
  
